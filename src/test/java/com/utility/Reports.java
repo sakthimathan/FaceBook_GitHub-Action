71afc7dd2cd6577caadd7daf3ba55408cd362d6c
mmathan161@gmail.com <mmathan161@gmail.com>
2024-03-24 16:42:03 +0530

    Reports methods added

---
+++ src/test/java/com/utility/Reports.java
@@ -0,0 +1,14 @@
+package com.utility;
+
+public class Reports {
+	
+	private void M1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
