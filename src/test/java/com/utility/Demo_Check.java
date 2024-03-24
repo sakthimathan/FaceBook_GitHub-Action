4f40fb50de531764e3b5c9c6240e766bb399061b
Manoth-2019 <manothkumar369@gmail.com>
2024-03-24 18:33:55 +0530

    rajini

--- src/test/java/com/utility/Demo_Check.java
+++ src/test/java/com/utility/Demo_Check.java
@@ -2,8 +2,10 @@ package com.utility;
 
 public class Demo_Check {
 	private void vijay() {
-		// TODO Auto-generated method stub
-
+		String s="aji";
+		System.out.println("iam vijay");
+		System.out.println("superstar");
+		System.out.println(s);
 	}
 	private void ajith() {
 		// TODO Auto-generated method stub
